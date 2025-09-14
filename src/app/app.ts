44ed67c2f107ecfe9123e80020cdd77c1ec676ce
KarenA1709 <kamontanon@ufpso.edu.co>
2025-09-13 20:00:25 -0500

    Agrego nuevo componente y modificaciones en app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NuevoComponente } from "./nuevo-componente/nuevo-componente";
+import { Online } from "./online/online";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NuevoComponente, Online],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('app-angular');
+  protected readonly title = 'Hola Angular';
 }
---
+++ src/app/online/online.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-online',
+  imports: [],
+  template: `
+    <p>
+      online works!
+    </p>
+  `,
+  styles: `
+  p{
+    color:blue;
+  }`
+})
+export class Online {
+
+}
