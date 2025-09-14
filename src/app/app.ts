e8f545c460b47930b619b8c637a7de2952dbb677
KarenA1709 <kamontanon@ufpso.edu.co>
2025-09-13 20:31:14 -0500

    Interpolacion

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NuevoComponente } from "./nuevo-componente/nuevo-componente";
 import { Online } from "./online/online";
+import { Interpolacion } from "./interpolacion/interpolacion";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NuevoComponente, Online],
+  imports: [RouterOutlet, NuevoComponente, Online, Interpolacion],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
